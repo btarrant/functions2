31f43a4aeb1c358f44edeb662ea41828386169cc
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 23:34:11 -0500

    Answered problem 9.

--- functions2.py
+++ functions2.py
@@ -55,17 +55,17 @@ def number_of_oceans_or_fingers_or_continents():
 print(number_of_oceans_or_fingers_or_continents())
 # >>> Output = 100, 10
 
-# #9
-# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-#     if b<c:
-#         return 7
-#     else:
-#         return 14
-#     return 3
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
+#9
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# >>> Output = 7, 14, 21
 
 # #10
 # def addition(b,c):
