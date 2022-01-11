5fc4602f22ec47da719b46df2d5883e56b1b543b
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 23:13:33 -0500

    Answered problem 2.

--- functions2.py
+++ functions2.py
@@ -4,11 +4,11 @@ def number_of_food_groups():
 print(number_of_food_groups())
 # >>> Output = 5
 
-# #2
-# def number_of_military_branches():
-#     return 5
-# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+#2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# >>> Output = Error
 
 # #3
 # def number_of_books_on_hold():
